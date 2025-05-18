a052e0bc039add4a83347350c3daf6bf56b2b78f
re-up-hq <hq@re-up.ph>
2025-05-18 15:13:50 +0800

    update:firestore-rules

--- src/lib/firebase/add-sub.ts
+++ src/lib/firebase/add-sub.ts
@@ -13,27 +13,18 @@ export async function addSub(doc_id: string, payload: Sub) {
   try {
     const ref = collection(db, "subs");
     const docRef = doc(ref, doc_id);
-    const docSnap = await getDoc(docRef);
 
-    console.log(doc_id, JSON.stringify(payload, null, 2));
+    await setDoc(docRef, {
+      id: doc_id,
+      user: payload.user,
+      affiliateId: payload.affiliateId,
+      device: payload.device,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
 
-    if (docSnap.exists()) {
-      console.log("Document already exists!");
-      return "exists";
-    } else {
-      // Document doesn't exist, add it
-      await setDoc(docRef, {
-        id: doc_id,
-        user: payload.user,
-        affiliateId: payload.affiliateId,
-        device: payload.device,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      });
-
-      console.log("Document added successfully!");
-      return "success";
-    }
+    console.log("Document added successfully!");
+    return "success";
   } catch (e) {
     console.error("Error adding document: ", e);
     return "failed";
